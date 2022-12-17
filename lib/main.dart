0abd8ea5b1fddaea4149c0465ff1ec34a06620e4
EPasiakos <57062196+EPasiakos@users.noreply.github.com>
2022-12-17 23:07:52 +0000

    Fixed comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-//import 'package:location/location.dart' hide LocationAccuracy;
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
