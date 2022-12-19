73f1ba2d299afd7e6c1e20b2730bcf27e1243f1b
Evangelos <evangelospasiakos@yahoo.com>
2022-12-19 13:23:31 +0000

    Improved readability and reusability of code by extracting buttons

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,6 @@ class _MapScreenState extends State<MapScreen> {
 
         // We use the layout builder to get the size of the screen and then we use that to determine the position of the buttons
         LayoutBuilder(
-          // padding: const EdgeInsets.all(15.0),
           builder: (context, constraints) {
             // If the screen is wider than 600, use a row layout
             if (constraints.maxWidth > 600) {
@@ -83,88 +82,9 @@ class _MapScreenState extends State<MapScreen> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        textStyle: style.textStyle,
-                        alignment: style.alignment,
-                        fixedSize: style.fixedSize,
-                        shape: style.shape,
-                        shadowColor: style.shadowColor,
-                        elevation: style.elevation,
-                        //Give the button a custom background color
-                        backgroundColor: MaterialStateProperty.all(Colors.blue),
-                      ),
-                      onPressed: () {
-                        // Generete random coordinates for the users location.
-                        LatLng userLocation = LatLng(generateRandomLatitude(),
-                            generateRandomLongitude());
-                        // Center the map on the new user's location.
-                        mapController
-                            .moveCamera(CameraUpdate.newLatLng(userLocation));
-                        // Update the marker's position and center the map on it when the user taps the button
-                        setState(() {
-                          _center = userLocation;
-                        });
-                        // add the new coordinates to the previousCoordinates map
-                        previousCoordinatesArray.add({
-                          'latitude': userLocation.latitude,
-                          'longitude': userLocation.longitude
-                        });
-                      },
-                      child: const Text('Teleport me to somewhere random'),
-                    ),
+                    teleportButton(),
                     const SizedBox(height: 30),
-                    ElevatedButton(
-                      style: style,
-                      onPressed: () async {
-                        // Request the user's permission to access their location
-                        await Geolocator.requestPermission();
-                        // Check if the user granted permission to access their location
-                        bool isLocationServiceEnabled =
-                            await Geolocator.isLocationServiceEnabled();
-
-                        if (isLocationServiceEnabled) {
-                          // Get the user's current location
-                          Position position =
-                              await Geolocator.getCurrentPosition(
-                                  desiredAccuracy: LocationAccuracy.high);
-                          LatLng userLocation =
-                              LatLng(position.latitude, position.longitude);
-                          // Center the map on the user's location
-                          mapController
-                              .moveCamera(CameraUpdate.newLatLng(userLocation));
-                          // Update the marker's position and center the map on it when the user taps the button
-                          setState(() {
-                            _center = userLocation;
-                          });
-
-                          // Generate the modal dialog that will show the user's current and previous coordinates
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              backgroundColor:
-                                  const Color.fromARGB(125, 33, 33, 33),
-                              titleTextStyle: TextStyle(
-                                color: Colors.white.withOpacity(0.8),
-                              ),
-                              contentTextStyle: TextStyle(
-                                color: Colors.white.withOpacity(0.8),
-                              ),
-                              title: const Text(
-                                "Current location",
-                                textAlign: TextAlign.center,
-                              ),
-                              content: Text(
-                                  "Latitude: ${position.latitude.toInt()}\n Longitude: ${position.longitude.toInt()}\n\n Previous\n\n ${generatePreviousCoordinatesString()}",
-                                  textAlign: TextAlign.center),
-                            ),
-                          );
-                        } else {
-                          // Handle the case where the user did not grant permission to access their location
-                        }
-                      },
-                      child: const Text('Bring me back home'),
-                    ),
+                    bringMebackHome(),
                   ],
                 ),
               );
@@ -176,94 +96,9 @@ class _MapScreenState extends State<MapScreen> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          textStyle: style.textStyle,
-                          fixedSize: style.fixedSize,
-                          shape: style.shape,
-                          shadowColor: style.shadowColor,
-                          elevation: style.elevation,
-                          //Give the button a custom background color
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.blue),
-                        ),
-                        onPressed: () {
-                          // Generete random coordinates for the users location.
-                          LatLng userLocation = LatLng(generateRandomLatitude(),
-                              generateRandomLongitude());
-                          // Center the map on the new user's location.
-                          mapController
-                              .moveCamera(CameraUpdate.newLatLng(userLocation));
-                          // Update the marker's position and center the map on it when the user taps the button
-                          setState(() {
-                            _center = userLocation;
-                          });
-                          // add the new coordinates to the previousCoordinates map
-                          previousCoordinatesArray.add({
-                            'latitude': userLocation.latitude,
-                            'longitude': userLocation.longitude
-                          });
-                        },
-                        child: const Text(
-                          'Teleport me to somewhere random',
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
+                      teleportButton(),
                       const SizedBox(height: 30),
-                      ElevatedButton(
-                        style: style,
-                        onPressed: () async {
-                          // Request the user's permission to access their location
-                          await Geolocator.requestPermission();
-                          // Check if the user granted permission to access their location
-                          bool isLocationServiceEnabled =
-                              await Geolocator.isLocationServiceEnabled();
-
-                          if (isLocationServiceEnabled) {
-                            // Get the user's current location
-                            Position position =
-                                await Geolocator.getCurrentPosition(
-                                    desiredAccuracy: LocationAccuracy.high);
-                            LatLng userLocation =
-                                LatLng(position.latitude, position.longitude);
-                            // Center the map on the user's location
-                            mapController.moveCamera(
-                                CameraUpdate.newLatLng(userLocation));
-                            // Update the marker's position and center the map on it when the user taps the button
-                            setState(() {
-                              _center = userLocation;
-                            });
-
-                            // Generate the modal dialog that will show the user's current and previous coordinates
-                            showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                backgroundColor:
-                                    const Color.fromARGB(125, 33, 33, 33),
-                                titleTextStyle: TextStyle(
-                                  color: Colors.white.withOpacity(0.8),
-                                ),
-                                contentTextStyle: TextStyle(
-                                  color: Colors.white.withOpacity(0.8),
-                                ),
-                                title: const Text(
-                                  "Current location",
-                                  textAlign: TextAlign.center,
-                                ),
-                                content: Text(
-                                    "Latitude: ${position.latitude.toInt()}\n Longitude: ${position.longitude.toInt()}\n\n Previous\n\n ${generatePreviousCoordinatesString()}",
-                                    textAlign: TextAlign.center),
-                              ),
-                            );
-                          } else {
-                            // Handle the case where the user did not grant permission to access their location
-                          }
-                        },
-                        child: const Text(
-                          'Bring me back home',
-                          textAlign: TextAlign.center,
-                        ),
-                      )
+                      bringMebackHome(),
                     ],
                   ),
                 ),
@@ -275,7 +110,118 @@ class _MapScreenState extends State<MapScreen> {
     ));
   }
 
-  // Function that loops through the previousCoordinatesArray and generates a string with the coordinates to be used in the modal dialog
+  // Calls the generateRandomLatitude and generateRandomLongitude functions to
+  // generate random coordinates, then moves the map to those coordinates and
+  // adds the coordinates to the previousCoordinatesArray.
+  ElevatedButton teleportButton() {
+    return ElevatedButton(
+      style: ButtonStyle(
+        textStyle: style.textStyle,
+        alignment: style.alignment,
+        fixedSize: style.fixedSize,
+        shape: style.shape,
+        shadowColor: style.shadowColor,
+        elevation: style.elevation,
+        //Give the button a custom background color
+        backgroundColor: MaterialStateProperty.all(Colors.blue),
+      ),
+      onPressed: () {
+        // Generete random coordinates for the users location.
+        LatLng userLocation =
+            LatLng(generateRandomLatitude(), generateRandomLongitude());
+        // Center the map on the new user's location.
+        mapController.moveCamera(CameraUpdate.newLatLng(userLocation));
+        // Update the marker's position and center the map on it when the user taps the button
+        setState(() {
+          _center = userLocation;
+        });
+        // add the new coordinates to the previousCoordinates map
+        previousCoordinatesArray.add({
+          'latitude': userLocation.latitude,
+          'longitude': userLocation.longitude
+        });
+      },
+      child: const Text('Teleport me to somewhere random'),
+    );
+  }
+
+  // Request the user's permission to access their location, then check if the
+  // user granted permission to access their location. If they did, get the user's
+  // current location and center the map on it. Then, generate the modal dialog
+  // that will show the user's current and previous coordinates.
+  ElevatedButton bringMebackHome() {
+    return ElevatedButton(
+      style: style,
+      onPressed: () async {
+        // Request the user's permission to access their location
+        await Geolocator.requestPermission();
+        // Check if the user granted permission to access their location
+        bool isLocationServiceEnabled =
+            await Geolocator.isLocationServiceEnabled();
+
+        if (isLocationServiceEnabled) {
+          // Get the user's current location
+          Position position = await Geolocator.getCurrentPosition(
+              desiredAccuracy: LocationAccuracy.high);
+          LatLng userLocation = LatLng(position.latitude, position.longitude);
+          // Center the map on the user's location
+          mapController.moveCamera(CameraUpdate.newLatLng(userLocation));
+          // Update the marker's position and center the map on it when the user taps the button
+          setState(() {
+            _center = userLocation;
+          });
+
+          // Generate the modal dialog that will show the user's current and previous coordinates
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              backgroundColor: const Color.fromARGB(125, 33, 33, 33),
+              titleTextStyle: TextStyle(
+                color: Colors.white.withOpacity(0.8),
+              ),
+              contentTextStyle: TextStyle(
+                color: Colors.white.withOpacity(0.8),
+              ),
+              title: const Text(
+                "Current location",
+                textAlign: TextAlign.center,
+              ),
+              // String that will be displayed in the modal dialog aligned to the center.
+              content: Text(
+                  "Latitude: ${position.latitude.toInt()}\n Longitude: ${position.longitude.toInt()}\n\n Previous\n\n ${generatePreviousCoordinatesString()}",
+                  textAlign: TextAlign.center),
+            ),
+          );
+        } else {
+          // Handle the case where the user did not grant permission to access their location
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              backgroundColor: const Color.fromARGB(125, 33, 33, 33),
+              titleTextStyle: TextStyle(
+                color: Colors.white.withOpacity(0.8),
+              ),
+              contentTextStyle: TextStyle(
+                color: Colors.white.withOpacity(0.8),
+              ),
+              title: const Text(
+                "Location services disabled",
+                textAlign: TextAlign.center,
+              ),
+              // String that will be displayed in the modal dialog aligned to the center.
+              content: const Text(
+                  "Please enable location services to use this feature",
+                  textAlign: TextAlign.center),
+            ),
+          );
+        }
+      },
+      child: const Text('Bring me back home'),
+    );
+  }
+
+  // Function that loops through the previousCoordinatesArray and generates a
+  // string with the coordinates to be used in the modal dialog
   String generatePreviousCoordinatesString() {
     String previousCoordinatesString = "";
     // Loop through the previousCoordinatesArray and generate a string with the coordinates formatted as Integers
